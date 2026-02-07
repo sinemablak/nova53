96dec35f05415812b50a8b56e0f7e0245aab4120
sinemablak <87177874+sinemablak@users.noreply.github.com>
2026-02-07 13:58:15 +0300

    Create script.js

---
+++ 01-dom-giris/ornek-kod/script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("button");
+const title = document.querySelector("h1");
+
+button.addEventListener("click", function () {
+    title.textContent = "DOM Öğreniyorum";
+});
